5503db4e769700faf2021d9c526dacd084f94f44
OAndreae <oliverandreae@outlook.com>
2021-03-14 17:43:26 +0000

    Added untracked files

---
+++ 1 Getting Started/Getting Started/HelloWorld/HelloWorld/WelcomePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Welcome", string.Format("Hello, {0}, how are you today?", name.Text) , "Fine, thanks", "I could be better");
+        }
+    }
+}
